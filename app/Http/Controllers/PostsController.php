0aabd036f251e58eb2acf313797a73fb684ce72a
Patrick Sweeney <blueshift9@gmail.com>
2019-01-10 20:59:06 -0500

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::query()->where('active','=',1)->orderByDesc('id')->get();
+        return view('posts', ['posts' => $posts]);
+    }
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xs-12">
+            <h1>Latest Posts</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        @foreach($posts as $post)
+            <div id="post-{{ $post->id }}" class="card col-xs-12  mb-5">
+                <img id="post-image-{{ $post->id }}" class="card-img-top img-fluid"
+                     src="https://placekitten.com/1024/230" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">{{ $post->description }}</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
