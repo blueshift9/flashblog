7112fb37e2b1711424abc9ec8635b1e5c43f41ea
Patrick Sweeney <blueshift9@gmail.com>
2019-01-24 20:39:40 -0500

    store a post working

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,14 +4,16 @@
 
     <div class="col-xs-12 col-md-6">
         <h2>Create New Post</h2>
-        <form>
+        <form method="post" action="{{ url('admin/posts') }}">
+            {{ csrf_field() }}
+
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Post Title">
+                <input type="text" class="form-control" id="title" name="title" aria-describedby="title" placeholder="Post Title">
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea id="description" class="form-control" placeholder="Description"></textarea>
+                <textarea id="description" class="form-control" name="description" placeholder="Description"></textarea>
             </div>
             Active
             <div class="form-check">
@@ -26,6 +28,7 @@
                     No
                 </label>
             </div>
+            <input type="hidden" name="user_id" value="1">
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
