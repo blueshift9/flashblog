ec7a9a8969daa2f894702e11bfcefb542b167654
Patrick Sweeney <blueshift9@gmail.com>
2019-01-28 19:23:55 -0500

    image upload works

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -38,7 +38,45 @@
 @push('scripts')
     <script>
         $(function() {
-            $('#description').summernote();
+            // Define function to open filemanager window
+            var lfm = function(options, cb) {
+                var route_prefix = (options && options.prefix) ? options.prefix : '/laravel-filemanager';
+                window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager', 'width=900,height=600');
+                window.SetUrl = cb;
+            };
+
+            // Define LFM summernote button
+            var LFMButton = function(context) {
+                var ui = $.summernote.ui;
+                var button = ui.button({
+                    contents: '<i class="note-icon-picture"></i> ',
+                    tooltip: 'Insert image with filemanager',
+                    click: function() {
+
+                        lfm({type: 'image', prefix: '/laravel-filemanager'}, function(lfmItems, path) {
+                            lfmItems.forEach(function (lfmItem) {
+                                context.invoke('insertImage', lfmItem.url);
+                            });
+                        });
+
+                    }
+                });
+                return button.render();
+            };
+            $('#description').summernote({
+                toolbar: [
+                    ['popovers', ['lfm']],
+                    ['style', ['bold', 'italic', 'underline', 'clear']],
+                    ['font', ['strikethrough', 'superscript', 'subscript']],
+                    ['fontsize', ['fontsize']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['height', ['height']],
+                ],
+                buttons: {
+                    lfm: LFMButton
+                }
+            });
 
         });
     </script>
