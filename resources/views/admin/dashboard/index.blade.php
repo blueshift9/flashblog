bb43cea55157c3ff03e15a46e4d5e537a029d2c5
Patrick Sweeney <blueshift9@gmail.com>
2019-01-29 20:24:22 -0500

    show a to-do on the front page

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,10 +4,21 @@
 
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-xs-12">
                 Welcome to your dashboard, @if(Auth::check())
                     {{Auth::user()->name}}
                 @endif!
+
+                <h3>Things to do</h3>
+                <ul>
+                    <li>update to 5.7</li>
+                    <li>Get a slug package, add slugs</li>
+                    <li>change Create post to be logged in user, not user 1</li>
+                    <li>js validator</li>
+                    <li>add a Create post button</li>
+                    <li>use description for excerpt, add actual body text to post</li>
+                    <li>look into a role plugin</li>
+                </ul>
             </div>
         </div>
     </div>
