d7ece08230d4d894db4f891df2b3cd092c09986a
Patrick Sweeney <blueshift9@gmail.com>
2020-03-10 22:00:41 -0400

    small styling changes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,32 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-xs-12">
-            <h1>Latest Posts</h1>
-        </div>
-    </div>
     <div class="">
+            <h1>Latest Posts</h1>
+    </div>
         @foreach($posts as $post)
             <div id="post-{{ $post->id }}" class="max-w rounded overflow-hidden shadow-lg my-16">
                 <img id="post-image-{{ $post->id }}" class="w-full"
                      src="https://placekitten.com/1024/230" alt="Card image cap">
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{{ $post->title }}</div>
+                    <div class="font-bold text-xl mb-2 post-title">{{ $post->title }}</div>
                     <p class="text-gray-700 text-base">
                         {!! $post->description !!}
                     </p>
                 </div>
-                {{--<div class="card-body">
-                    <h5 class="card-title">{{ $post->title }}</h5>
-                    <em>
-                      by  {{ $post->user->name }} on {{ $post->created_at->format('m-d-Y  H:i') }}
-                    </em>
-                    <p class="card-text">{!! $post->description !!}</p>
-                    <a href="#" class="btn btn-primary">Read</a>
+                <!-- use below for tags -->
+                {{--<div class="px-6 py-4">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
                 </div>--}}
             </div>
         @endforeach
-    </div>
 </div>
 @endsection
