b1bd1bf25ca992786d0c317b787af3944de3270b
Patrick Sweeney <blueshift9@gmail.com>
2020-03-16 15:14:53 -0400

    register template, will still need some cleanup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,92 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
+    @extends('layouts.login')
+
+@section('content')
+
+    <div class="w-full max-w-xs ">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('register') }}"
+              aria-label="{{ __('Register') }}">
+
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    {{ __('Name') }}
+                </label>
+                <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline {{ $errors->has('name') ? ' is-invalid' : '' }}"/>
+                @if ($errors->has('name'))
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                @endif
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    {{ __('Email') }}
+                </label>
+
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                @endif
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password"
+                       id="email" type="email" placeholder="******************" required>
+                {{--
+                                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                --}}
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    {{ __('Password') }}
+                </label>
+
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                @endif
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password"
+                       id="password" type="password" placeholder="******************" required>
+                {{--
+                                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                --}}
+            </div>
+
+            <div class="mb-6">
+                <label for="password-confirm" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Confirm Password') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password-confirm" type="password" class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password_confirmation" required>
+                </div>
+            </div>
+
+
+            <div class="flex items-center justify-between">
+
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            {{ __('Register') }}
+                        </button>
+                    </div>
+                </div>
+
+
+
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                   href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            </div>
+        </form>
+
+    </div>
+
+@endsection
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -53,21 +139,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
