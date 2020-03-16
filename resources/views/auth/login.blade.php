98e696ba47cbc55ed9f66b6733b80590f0c0add9
Patrick Sweeney <blueshift9@gmail.com>
2020-03-16 14:52:36 -0400

    convert login to Tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+    <div class="w-full max-w-xs ">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}"
+              aria-label="{{ __('Login') }}">
+            {{ __('Login') }}
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    {{ __('E-Mail Address') }}
+                </label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus
+                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline {{ $errors->has('email') ? ' is-invalid' : '' }}"/>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                @endif
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    {{ __('Password') }}
+                </label>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @endif
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password"
+                       id="password" type="password" placeholder="******************" required>
+                {{--
+                                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                --}}
             </div>
-        </div>
+            <div class="mb-6">
+                <label class="form-check-label" for="remember">
+                    {{ __('Remember Me') }}
+                </label>
+                <input class="form-check-input" type="checkbox" name="remember"
+                       id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+
+            </div>
+
+            <div class="flex items-center justify-between">
+                <button type="submit"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                    {{ __('Login') }}
+                </button>
+
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                   href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+
+        </p>
     </div>
-</div>
+
 @endsection
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-300">
+{{--<div class="border-b border-indigo-darkest bg-teal py-4 px-20">
+    <div class="container mx-auto">
+        <nav class="flex items-center justify-between flex-wrap">
+            <div class="flex items-center flex-no-shrink text-dark mr-6">
+                <span class="font-semibold text-xl tracking-tight">Flashblog</span>
+            </div>
+            <div class="block sm:hidden">
+                <button class="navbar-burger flex items-center px-3 py-2 border rounded text-gray border-gray-700 hover:text-white hover:border-white">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div id="main-nav" class="w-full flex-grow sm:flex items-center sm:w-auto hidden">
+                <div class="text-sm sm:flex-grow">
+                    <a href="{{ route('login') }}" class="no-underline font-bold block mt-4 sm:inline-block sm:mt-0 text-grey-lighter hover:text-grey-light mr-4">
+                        Admin
+                    </a>
+
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>--}}
+<div class="flex items-center justify-center h-screen">
+
+        @yield('content')
+
+    </div>
+</body>
+</html>
