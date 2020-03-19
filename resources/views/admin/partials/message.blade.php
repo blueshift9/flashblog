3fa37df93a4feba9de6e5395f43bdb96723524e3
Patrick Sweeney <blueshift9@gmail.com>
2020-03-19 12:59:04 -0400

    pretty up warnings and the like

--- resources/views/admin/partials/message.blade.php
+++ resources/views/admin/partials/message.blade.php
@@ -1,42 +1,68 @@
 @if ($message = Session::get('success'))
-    <div class="alert alert-success alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500">
+
+        <span class="inline-block align-middle mr-8">
+            <b class="capitalize">Success!</b> {{ $message }}
+        </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span>×</span>
+        </button>
     </div>
 @endif
 
 
 @if ($message = Session::get('error'))
-    <div class="alert alert-danger alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-500-500">
+
+        <span class="inline-block align-middle mr-8">
+            <b class="capitalize">There was a problem!</b> {{ $message }}
+        </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span>×</span>
+        </button>
     </div>
 @endif
 
 
 @if ($message = Session::get('warning'))
-    <div class="alert alert-warning alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-yellow-500-500">
+
+        <span class="inline-block align-middle mr-8">
+            <b class="capitalize">Warning!</b> {{ $message }}
+        </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span>×</span>
+        </button>
     </div>
 @endif
 
+{{ $message }}
 
 @if ($message = Session::get('info'))
-    <div class="alert alert-info alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-blue-500">
+
+        <span class="inline-block align-middle mr-8">
+            <b class="capitalize">Success!</b> {{ $message }}
+        </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span>×</span>
+        </button>
     </div>
 @endif
 
 
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-500">
+        <span class="inline-block align-middle mr-8">
+            <b>There was at least one error: </b><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span>×</span>
+        </button>
     </div>
 @endif
