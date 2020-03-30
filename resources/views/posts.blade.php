108bc29dc0e693ea0ee41b35f83b059789824271
Patrick Sweeney <blueshift9@gmail.com>
2020-03-30 14:33:30 -0400

    show message if no results

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        @foreach($posts as $post)
+        @forelse($posts as $post)
             <div id="post-{{ $post->id }}" class="max-w rounded overflow-hidden shadow-lg my-16 mx-2 bg-white">
                 <img id="post-image-{{ $post->id }}" class="w-full"
                         src="https://placekitten.com/1024/230" alt="Card image cap">
@@ -34,7 +34,17 @@
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>--}}
                 </div>
             </div>
-        @endforeach
+        @empty
+            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-16 mx-2" role="alert">
+                <div class="flex">
+                    <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                    <div>
+                        <p class="font-bold">No results!</p>
+                        <p class="">We found no results for <span class="font-bold">{{ request()->query('search') }}</span>.</p>
+                    </div>
+                </div>
+            </div>
+        @endforelse
 
         {{ $posts->appends(['search' => request()->query('search')])->links() }}
     </div>
