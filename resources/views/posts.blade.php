9a5c6d7ec49a000652d149df6c26a68f6c0a9d0f
Patrick Sweeney <blueshift9@gmail.com>
2020-03-30 14:14:27 -0400

    query fix

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
         @foreach($posts as $post)
             <div id="post-{{ $post->id }}" class="max-w rounded overflow-hidden shadow-lg my-16 mx-2 bg-white">
                 <img id="post-image-{{ $post->id }}" class="w-full"
-                     src="https://placekitten.com/1024/230" alt="Card image cap">
+                        src="https://placekitten.com/1024/230" alt="Card image cap">
                 <div class="px-6 py-4">
-                    @isset($post->excerpt)<p class="uppercase tracking-wide text-sm font-bold text-gray-700">{{ $post->excerpt}}</p>@endisset
+                    @isset($post->excerpt)
+                        <p class="uppercase tracking-wide text-sm font-bold text-gray-700">{{ $post->excerpt}}</p>@endisset
                     <div class="font-bold text-4xl mb-2 post-title">
                         <a href="posts/{{ $post->slug }}">{{ $post->title }}</a>
-                        </div>
+                    </div>
                 </div>
                 <!-- use below for tags -->
                 <div class="px-4 pt-3 pb-4 border-t border-b border-gray-300 bg-gray-100">
@@ -27,7 +28,7 @@
                 </div>
                 <div class="px-6 py-4">
                     @foreach($post->tags as $tag)
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{{ $tag->name }}</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{{ $tag->name }}</span>
                     @endforeach
                     {{--<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>--}}
@@ -35,6 +36,6 @@
             </div>
         @endforeach
 
-    {{ $posts->links() }}
-</div>
+        {{ $posts->appends(['search' => request()->query('search')])->links() }}
+    </div>
 @endsection
